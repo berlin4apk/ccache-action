76558e05b4f97648be3ad5768cd850b86803578a
berlin4apk <83521068+berlin4apk@users.noreply.github.com>
2023-04-14 04:14:14 +0200

    Update restore.ts

--- src/restore.ts
+++ src/restore.ts
@@ -110,7 +110,7 @@ async function installCcacheMac() : Promise<void> {
     "darwin",
     "tar.gz",
     // sha256sum of ccache
-    "FFFFFFFFFFFFFfffffffffffffffFFfffffffFFF",
+    "FFFFFFFFFFFFFfffffffffffffffffFFfffffffFFF",
     "/usr/local/bin/",
     "ccache"
     );
