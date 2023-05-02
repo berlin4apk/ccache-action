1f90e99e563f6bc509ce3c718b4c6285c041e7ea
berlin4apk <83521068+berlin4apk@users.noreply.github.com>
2023-05-02 15:17:55 +0200

    update src/update-sccache-symlinks.sh

--- src/update-sccache-symlinks.sh
+++ src/update-sccache-symlinks.sh
@@ -51,6 +51,7 @@ PATH=\${PATH/%":\$SCCACHE_WRAPPER_BINDIR"/} # delete any instance in the at the 
 \${SCCACHE_BIN} \${COMPILER} "$@"
 \${SCCACHE_BIN} \${COMPILER} "\\$\\@"
 \${SCCACHE_BIN} \${COMPILER} "\$\@"
+\${SCCACHE_BIN} \${COMPILER} "\$@"
 EOF
 chmod 755 "./\${COMPILER}"
 done
