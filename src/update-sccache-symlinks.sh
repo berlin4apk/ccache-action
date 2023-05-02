aab96f1e4aed1a3f08580390df36d86941d15542
berlin4apk <83521068+berlin4apk@users.noreply.github.com>
2023-05-02 14:57:01 +0200

    update src/update-sccache-symlinks.sh

--- src/update-sccache-symlinks.sh
+++ src/update-sccache-symlinks.sh
@@ -38,24 +38,24 @@ export -p | grep -i deb
 [ -d /usr/local/lib/sccache/ ] || $Sudo mkdir -p /usr/local/lib/sccache/
 cat <<EOF1 | $SudoE tee /usr/local/lib/sccache/sccache-wrapper
 #!/usr/bin/env bash
-SCCACHE_BIN="$(command -v sccache || echo sccache )"
-cd "$(dirname "$0")"
+SCCACHE_BIN="\$(command -v sccache || echo sccache )"
+cd "\$(dirname "$0")"
 for COMPILER in "c++" "c89" "c99" "cc" "clang" "clang++" "cpp" "g++" "gcc" "rustc" "x86_64-pc-linux-gnu-c++" "x86_64-pc-linux-gnu-cc" "x86_64-pc-linux-gnu-g++" "x86_64-pc-linux-gnu-gcc" "arm-none-eabi-c++" "arm-none-eabi-cc" "arm-none-eabi-g++" "arm-none-eabi-gcc" "aarch64-linux-gnu-c++" "aarch64-linux-gnu-cc" "aarch64-linux-gnu-g++" "aarch64-linux-gnu-gcc" "arm-none-eabi-c++" "arm-none-eabi-cc" "arm-none-eabi-g++" "arm-none-eabi-gcc"; do
-cat > "./${COMPILER}" <<-EOF
+cat > "./\${COMPILER}" <<-EOF
 #!/bin/bash
 SCCACHE_WRAPPER_BINDIR="\$(dirname \${BASH_SOURCE[0]})"  # Intentionally don't resolve symlinks
 PATH=\${PATH//":\$SCCACHE_WRAPPER_BINDIR:"/":"} # delete any instances in the middle
 PATH=\${PATH/#"\$SCCACHE_WRAPPER_BINDIR:"/} # delete any instance at the beginning
 PATH=\${PATH/%":\$SCCACHE_WRAPPER_BINDIR"/} # delete any instance in the at the end
-# /usr/bin/sccache ${COMPILER} "\$@"
-${SCCACHE_BIN} ${COMPILER} "\$@"
+# /usr/bin/sccache \${COMPILER} "\$@"
+\${SCCACHE_BIN} \${COMPILER} "\$@"
 EOF
 chmod 755 "./${COMPILER}"
 done
 EOF1
 $Sudo chmod 755 /usr/lib/sccache/sccache-wrapper || $Sudo chmod 755 /usr/local/lib/sccache/sccache-wrapper
 $SudoE /usr/lib/sccache/sccache-wrapper || $SudoE /usr/local/lib/sccache/sccache-wrapper
-ls -latr /usr/lib/sccache/ /usr/local/lib/sccache/ ||:
+ls -latr /usr/lib/sccache/ /usr/local/lib/sccache/ /usr/local/bin/ ||:
 
 
 # Prepend ccache into the PATH
