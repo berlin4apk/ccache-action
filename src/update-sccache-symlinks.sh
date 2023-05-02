b0131a95d070798015353cf34736161fa5ae169c
berlin4apk <83521068+berlin4apk@users.noreply.github.com>
2023-05-02 16:46:30 +0200

    update src/update-sccache-symlinks.sh

--- src/update-sccache-symlinks.sh
+++ src/update-sccache-symlinks.sh
@@ -67,14 +67,14 @@ cat <<'Endofmessage' | $SudoE tee /usr/local/lib/sccache/sccache-wrapper
 # cat <<EOF1 | $SudoE tee /usr/local/lib/sccache/sccache-wrapper
 #!/usr/bin/env bash
 set -vx
-SCCACHE_BIN="\$(command -v sccache || echo sccache )"
+SCCACHE_BIN="$(command -v sccache || echo sccache )"
 DIRNAME=$(dirname "$0")
 #cd "\$(dirname "\$PWD\$0")"
 cd "$DIRNAME"
 for COMPILER in "c++" "c89" "c99" "cc" "clang" "clang++" "cpp" "g++" "gcc" "rustc" "x86_64-pc-linux-gnu-c++" "x86_64-pc-linux-gnu-cc" "x86_64-pc-linux-gnu-g++" "x86_64-pc-linux-gnu-gcc" "arm-none-eabi-c++" "arm-none-eabi-cc" "arm-none-eabi-g++" "arm-none-eabi-gcc" "aarch64-linux-gnu-c++" "aarch64-linux-gnu-cc" "aarch64-linux-gnu-g++" "aarch64-linux-gnu-gcc" "arm-none-eabi-c++" "arm-none-eabi-cc" "arm-none-eabi-g++" "arm-none-eabi-gcc"; do
 #cat > "./\${COMPILER}" <<-EndofScript
 set -vx
-cat > "\${COMPILER}" <<-EndofScript
+cat > "./${COMPILER}" <<-EndofScript
 #!/bin/bash
 SCCACHE_WRAPPER_BINDIR="\$(dirname \${BASH_SOURCE[0]})"  # Intentionally don't resolve symlinks
 PATH=\${PATH//":\$SCCACHE_WRAPPER_BINDIR:"/":"} # delete any instances in the middle
